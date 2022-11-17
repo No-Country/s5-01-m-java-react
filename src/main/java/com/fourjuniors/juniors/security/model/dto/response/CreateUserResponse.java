b226920b7ac7792337a120f4da79c561b4242778
MartinKun <martinkunbrc@hotmail.com>
2022-11-17 12:02:00 -0300

    refactor package model

---
+++ src/main/java/com/fourjuniors/juniors/security/model/dto/response/CreateUserResponse.java
@@ -0,0 +1,24 @@
+package com.fourjuniors.juniors.security.model.dto.response;
+
+import com.fourjuniors.juniors.security.entity.Role;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateUserResponse {
+
+    private String username;
+    private String email;
+    private String password;
+    private String lastname;
+    private String portfolio;
+    private String profile;
+    private String experience;
+    private String stack;
+    private List<Role> roles;
+}
