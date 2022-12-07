36be83df78a6bee0a896310eedcfc5a7b930f3cd
florencia <flor_camilal@hotmail.com>
2022-12-07 10:00:36 -0300

    Subo cambios form register

--- frontend/src/components/FormRegister.jsx
+++ frontend/src/components/FormRegister.jsx
@@ -2,8 +2,47 @@ import React from "react";
 import logo from '../assets/img/LOGO.png'
 import '../pages/register/register.css';
 import BtnRed from "./buttons/BtnRed";
+import axios from "axios";
+import { useState } from "react";
 
-export default function FormRegister() {   
+export default function FormRegister() { 
+    const url = 'https://s5-01-m-java-react-production.up.railway.app/auth/signup'
+    const [data, setData] = useState({
+        email:"",
+        password:"",
+        name:"",
+        lastname:"",
+        profile:"",
+        stack:"",
+        portfolio:"",
+        experience:""
+    })
+
+    function submit(e){
+        e.preventDefault();
+        axios.post(url,{
+            email:data.email,
+            password:data.password,
+            name:data.name,
+            lastname:data.lastname,
+            profile:data.profile,
+            stack:data.stack,
+            portfolio:data.portfolio,
+            experience:data.experience
+        })
+        .then(res =>{
+            console.log(res.data)
+        })
+    }
+
+    function handle(e){
+        const newData= {...data}
+        newData[e.target.id] = e.target.value
+        setData(newData)
+        console.log(newData)
+    }
+    
+
     return (
         <div className="containerRegister">
 
@@ -15,34 +54,35 @@ export default function FormRegister() {
                 <p className="textRegister">¿Eres nuevo por aquí?</p>
                 <p className="data">Completa tus datos.</p>
             </div>
-
+        <form onSubmit={(e) => submit(e)}>
             <div className="containerForm">
                 <label htmlFor="">nombre</label>
-                <input type="text" className="margin" placeholder="John "/>
+                <input onChange={(e)=>handle(e)} id="name" value={data.name} type="text" className="margin" placeholder="John " />
                 <label htmlFor="">apellido</label>
-                <input type="text" className="margin"placeholder="Doe"/>
+                <input onChange={(e)=>handle(e)} id="lastname" value={data.lastname}  type="text" className="margin"placeholder="Doe"/>
                 <label htmlFor="">portfolio</label>
-                <input type="url" className="margin" placeholder="https://www.behance.net/johndoe"/>
+                <input onChange={(e)=>handle(e)} id="portfolio" value={data.portfolio} type="url" className="margin" placeholder="https://www.behance.net/johndoe"/>
                 <label htmlFor="">perfil</label>
-                <input type="text" className="margin"/>
+                <input onChange={(e)=>handle(e)} id="profile" value={data.profile} type="text" className="margin"/>
                 <label htmlFor="">experiencia</label>
-                <input type="text" className="margin"/>
+                <input onChange={(e)=>handle(e)} id="experience" value={data.experience} type="text" className="margin"/>
                 <label htmlFor="">stack de tecnologias</label>
-                <input type="text" className="margin"/>
+                <input onChange={(e)=>handle(e)} id="stack" value={data.stack} type="text" className="margin"/>
                 <label htmlFor="">e-mail</label>
-                <input type="email" className="margin" placeholder="johndoe@gmail.com"/>
+                <input onChange={(e)=>handle(e)} id="email" value={data.email}  type="email" className="margin" placeholder="johndoe@gmail.com"/>
                 <label htmlFor="">contraseña</label>
-                <input type="password" />
+                <input onChange={(e)=>handle(e)} id="password" value={data.password} type="password" />
                 <div className="passwordRegister">
                     <p>La constraseña debe tener al menos 8 <br /> caracteres.</p> 
                 </div>
                 <label htmlFor="">repetir contraseña</label>
-                <input type="password" className="margin"/>
+                <input onChange={(e)=>handle(e)} id="password" value={data.password}  type="password" className="margin"/>
 
                 <div className='btnRed'>
-                    <BtnRed label='REGISTRARSE' color='#F62223' width='160px'/>
+                    <BtnRed label='REGISTRARSE' color='#F62223' width='160px' />
                 </div>
             </div>
+        </form>
 
         </div>
 
